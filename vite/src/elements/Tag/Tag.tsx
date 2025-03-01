2fd5024ac4a32b4465594f1e54f02e1423198cea
BuckUbel <buckubel@gmail.com>
2025-03-01 08:55:47 +0100

    Enhance Tag component with new styles and category support
    Updated SCSS to refine styling, including darker color variants and hover effects with brightness filter. Added `category` prop in the Tag component to support primary, secondary, and tertiary variations. Improved close icon sizing and positioning for better UI consistency.

--- vite/src/elements/Tag/Tag.tsx
+++ vite/src/elements/Tag/Tag.tsx
@@ -1,16 +1,17 @@
 import React, {PropsWithChildren, useState} from "react";
-import {SimpleSetState, Status, UseAppStateType} from "../../types/default.ts";
+import {SimpleSetState, Status, UseAppStateType, UseState} from "../../types/default.ts";
 import {Icon} from "@iconify/react";
 
 interface TagProps {
   status?: Status
   active?: boolean;
-  state?: UseAppStateType<boolean>
+  state?: UseState<boolean> | UseAppStateType<boolean>
   onClick?: (newValue: boolean) => void;
+  category?: "primary" | "secondary" | "tertiary";
 }
 
 function Tag(props: PropsWithChildren<TagProps>) {
-  const {state, status, active = true, onClick} = props;
+  const {state, status, active = true, onClick, category} = props;
   const ownState = useState<boolean>(active)
   let isActive = ownState[0];
   let setIsActive: SimpleSetState<boolean> = ownState[1];
@@ -19,14 +20,14 @@ function Tag(props: PropsWithChildren<TagProps>) {
     setIsActive = state[1];
   }
 
-  return <div className={`tag ${status ?? ""}${isActive ? " active " : ""}`}
+  return <div className={`tag ${status ?? ""}${isActive ? " active " : ""} ${category ?? ""}`}
               onClick={() => {
                 const newValue = !isActive;
                 if (!!onClick) onClick(newValue);
                 if (!!setIsActive) setIsActive(newValue);
               }}>
     {props.children}
-    {isActive && !!setIsActive && <Icon icon={"material-symbols:close-small-rounded"} width="24" height="24"/>}
+    {isActive && !!setIsActive && <Icon className={"icon-close"} icon={"material-symbols:close-small-rounded"} width="24" height="24"/>}
   </div>
 }
 
