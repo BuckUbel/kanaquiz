4acbb906eab70c3ff6c8ffbb175637aafc2fcc93
BuckUbel <buckubel@gmail.com>
2025-03-01 08:54:59 +0100

    Add new kana and kanji SVG icons with updated references
    New SVG icons for kana and kanji, such as "kana dictionary" and "kanji tree," have been added, and the references in `customIcons.ts` have been updated accordingly. Also included are two new GIF animations for kana.

--- vite/src/helper/customIcons.ts
+++ vite/src/helper/customIcons.ts
@@ -1,11 +1,19 @@
-import kanaQuestionSvg from "../assets/kanaQuestion.svg";
+import kanaQuestionSvg from "../assets/svg/kanaQuestion.svg";
+import kanaDictionarySvg from "../assets/svg/kanaDictionary.svg";
+import kanaDictionaryOutlineSvg from "../assets/svg/kanaDictionaryOutline.svg";
+import kanaHiraganaASvg from "../assets/svg/kanaHiraganaA.svg";
+import kanaRomajiASvg from "../assets/svg/kanaRomajiA.svg";
+import kanjiTreeSvg from "../assets/svg/kanjiTree.svg";
 import {addIcon} from '@iconify/react';
 import {extractBodyFromSvg} from "./extractBodyFromSvg.ts";
 
 export function addCustomIcons() {
-  addIcon('kana-question', {
-    body: extractBodyFromSvg(kanaQuestionSvg),
-    width: 24, // Breite des Icons
-    height: 24 // Höhe des Icons
-  });
+  console.log(kanaDictionarySvg);
+
+  addIcon('kana-question', {body: extractBodyFromSvg(kanaQuestionSvg), width: 24, height: 24});
+  addIcon('kana-dictionary', {body: extractBodyFromSvg(kanaDictionarySvg), width: 25, height: 25});
+  addIcon('kana-dictionary-outline', {body: extractBodyFromSvg(kanaDictionaryOutlineSvg), width: 25, height: 25});
+  addIcon('kana-hiragana-a', {body: extractBodyFromSvg(kanaHiraganaASvg), width: 24, height: 24});
+  addIcon('kana-romaji-a', {body: extractBodyFromSvg(kanaRomajiASvg), width: 24, height: 24});
+  addIcon('kanji-tree', {body: extractBodyFromSvg(kanjiTreeSvg), width: 48, height: 48});
 }
